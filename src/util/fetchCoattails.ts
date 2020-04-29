0d7aec509b64fa021c914853967bf92309f361b0
Jason Ma <jason.ma@airbnb.com>
2020-04-29 14:48:42 -0700

    Refactor fetch

---
+++ src/util/fetchCoattails.ts
@@ -0,0 +1,22 @@
+const COATTAILS_HOSTNAME = process.env.COATTAILS_HOSTNAME ? process.env.COATTAILS_HOSTNAME : "http://127.0.0.1:8080"
+
+export function post(endpoint, initPayload, succCb, errCb) {
+  let headers = {
+    "Content-Type": "application/json",
+  };
+  let init = Object.assign({ headers, method: "POST", credentials: 'include' }, initPayload)
+  return fetch(COATTAILS_HOSTNAME + endpoint, init)
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+      console.log("res: ", res)
+      throw Error("internal server error")
+    })
+    .then(succCb)
+    .catch(errCb)
+}
+
+export function get(endpoint, initPayload, succCb, errCb) {
+  console.log("hello", COATTAILS_HOSTNAME)
+}
